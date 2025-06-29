373050491adc66ff6d2082d5a558e70891f102be
BIJARNIARANSINGH <bijarniaransingh@gmail.com>
2025-06-29 08:37:58 +0530

    error fixed

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => {
   console.log('MongoDB connected');
-  app.listen(PORT, () => console.log(Server running on port ${PORT}));
+  app.listen(PORT, () => console.log('Server running on port ${PORT}'));
 })
 .catch(err => console.error(err));
