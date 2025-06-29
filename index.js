0db82d6afbd679915eef881f300533fb9d20804f
BIJARNIARANSINGH <bijarniaransingh@gmail.com>
2025-06-29 09:54:21 +0530

    all files rewrite

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ mongoose.connect(process.env.MONGO_URI, {
   // Start server
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
-    console.log(Server started on port ${PORT});
+    console.log('Server started on port ${PORT}');
   });
 
 })
