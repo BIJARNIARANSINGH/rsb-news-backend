42f1cc66ff1abf755fa6c1c3981ebc2a448de0a0
BIJARNIARANSINGH <bijarniaransingh@gmail.com>
2025-06-30 07:50:23 +0530

    Initial backend push

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGO_URI, {
   console.log('MongoDB connected');
 
   // Start server
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log('Server started on port ${PORT}');
   });
