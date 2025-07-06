1bc0598b9ff5ee4e3fdd90429da3b358aa3384df
BIJARNIARANSINGH <bijarniaransingh@gmail.com>
2025-07-06 09:39:11 +0530

    All Fixed

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://rsb-news-frontend.vercel.app'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Serve static uploads (images, videos)
